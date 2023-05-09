17182620fecd1b7ce20669a16c3e1d342b5071b6
gajendra17 <99756849+gajendra17@users.noreply.github.com>
2023-05-09 15:04:52 +0530

    Update//6

--- initial.sv
+++ initial.sv
@@ -3,8 +3,13 @@ module ass;
     return a*b;
   endfunction
     bit [3:0] result=0;
-    initial begin 
+  bit status,comp;
+    initial begin
+      
       result =mul(3'b111,3'b010);
-      $display ("result=%0d",result);
+      comp=14;
+      status=(comp==result);
+      $display ("result=%0d and ststus=%d",result,status);
+      
     end 
     endmodule //2
