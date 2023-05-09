3bacf98116ebb6a80d0b21c7527a840d9d4385bc
gajendra17 <99756849+gajendra17@users.noreply.github.com>
2023-05-09 15:20:26 +0530

    Update//2

--- init2.sv
+++ init2.sv
@@ -1,1 +1,16 @@
 //edit2
+module multi
+(
+    input [3:0] a,b,
+  output reg [4:0] multi,
+    input clk
+  );
+  
+  
+  always@(posedge clk)
+    begin
+      sum <= a * b;
+    end
+   
+   
+endmodule
