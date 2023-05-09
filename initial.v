1978f5c7a5d6871d32c2dab4af8dd8e4c53c6e35
gajendra17 <99756849+gajendra17@users.noreply.github.com>
2023-05-09 14:47:55 +0530

    Update//4

--- initial.v
+++ initial.v
@@ -1,10 +1,10 @@
-module mult;
-  function bit [3:0] mm(bit [2:0]a,b);
-    return a*b;
-   endfunction
-    bit [3:0] result=0;
-    initial begin
-      result=mm(3'b111,3'b101);
-      $display("result=%0d",result);
-    end
-    endmodule
+module ass;
+  function bit [3:0] mul( bit [2:0]a,b);
+    return a*b;
+  endfunction
+    bit [3:0] result=0;
+    initial begin 
+      result =mul(3'b111,3'b010);
+      $display ("result=%0d",result);
+    end 
+    endmodule //2
