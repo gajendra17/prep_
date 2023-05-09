07976b6e9c391cd6272a269345ec76fab2ccdaf0
gajendra17 <99756849+gajendra17@users.noreply.github.com>
2023-05-09 15:35:47 +0530

    Update initial.sv

--- initial.sv
+++ initial.sv
@@ -4,20 +4,12 @@ module ass;
   endfunction
     bit [3:0] result=0;
   bit status;
- // bit [3:0] comp = 4'b1110;
-    initial begin
-      
-      result =mul(3'b111,3'b010);
-      comp=14;
-      status=(comp==result);
-      $display ("result=%0d and ststus=%d",result,status);
-      
-    end
-  
-  initial
-    multi dut (.b(b), .a(a), 
-  
-  
-  
-   begin
-    endmodule //2
+  initial begin
+    multi dut(.b(b), .a(a), .multi(result));
+  a=3'b111;
+  b=3'b010;
+   result =mul(3'b111,3'b010);
+  status=(multi==result);
+    $display ("result=%0d and ststus=%d",result,status);
+  end
+endmodule //2
